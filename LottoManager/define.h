fa98ef5cb1699ef7f3d2e489c45f4d7e8fac103d
jandroid.kim <vvv30612@naver.com>
2020-03-29 17:50:09 +0900

    add define.h

---
+++ LottoManager/define.h
@@ -0,0 +1,1 @@
+#define NUM_OF_ALLCASE (800)
--- LottoManager/main.cpp
+++ LottoManager/main.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include "define.h"
 
 int main()
 {
 	int a;
 	scanf("%d", &a);
-	printf("%d", a);
+	printf("%d", NUM_OF_ALLCASE);
 
 	//test
 
